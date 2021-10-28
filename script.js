45ab579f07abaa540160ca03494f670839f2cbb7
Tomás Vazquez <78186062+vazquezcabj21@users.noreply.github.com>
2021-10-27 22:13:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+import hamburguerMenu from "./menu-hamburguesa.js";
+
+const d = document;
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    hamburguerMenu(".panel-btn", ".panel", ".menu a")
+    
+})
